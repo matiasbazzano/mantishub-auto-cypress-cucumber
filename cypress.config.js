23f7915efc7e3b2c3794c259c42bf1280a85fda6
Matias Bazzano <matiasbazzano19@gmail.com>
2024-07-07 19:11:23 -0300

    pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,37 @@
 const { defineConfig } = require('cypress');
+const cypressOnFix = require('cypress-on-fix');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
-  plugins: [
-    createBundler(),
-  ],
   e2e: {
     async setupNodeEvents(on, config) {
-      const esbuildOptions = await createEsbuildPlugin(config);
+      on = cypressOnFix(on);
+      require('mochawesome-report-generator/plugin')(on);
       
-      on('file:preprocessor', addCucumberPreprocessorPlugin(config));
-      on('file:preprocessor', createBundler(esbuildOptions));
-
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+      on("file:preprocessor", bundler);
+      
+      await addCucumberPreprocessorPlugin(on, config);
+      
       return config;
     },
-    reporter: 'cucumber',
+    reporter: 'mochawesome',
     reporterOptions: {
-      cucumber: {
-        output: 'cypress/reports/cucumber/cucumber_report.json',
-        format: 'json',
-        testDir: 'cypress/e2e/features',
-      },
+      reportDir: 'cypress/reports/mochawesome',
+      overwrite: false,
+      html: true,
+      json: false
     },
     specPattern: [
-      'cypress/e2e/features/**/*.feature',
+      "cypress/e2e/features/ui/*.feature",
+      "cypress/e2e/features/api/*.feature"
     ],
-    baseUrl: 'https://cucumber-cypress02.mantishub.io/',
-    stepDefinitions: 'cypress/support/step_definitions/*.{js,ts}',
+    baseUrl: "https://cucumber-cypress02.mantishub.io/",
+    stepDefinitions: "cypress/support/step_definitions/*.{js,ts}",
     defaultCommandTimeout: 20000,
   },
 });
