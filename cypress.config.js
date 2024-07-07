83f16331f5a00acb3effe2536764954fb09e253b
Matias Bazzano <matiasbazzano19@gmail.com>
2024-07-07 14:07:11 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 const cypressOnFix = require('cypress-on-fix');
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       on = cypressOnFix(on);
       require('cypress-mochawesome-reporter/plugin')(on);
+      
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
       });
       on("file:preprocessor", bundler);
+      
       await addCucumberPreprocessorPlugin(on, config);
+      
       return config;
     },
     reporter: 'cypress-mochawesome-reporter',
